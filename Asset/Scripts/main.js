668c77699357685bc58c2dd5b52693cc90031c86
Nabil Fadhiilah <nabil.10518027@mahasiswa.unikom.ac.id>
2022-02-04 23:23:48 +0700

    update
    pemberian ellipsis, counter, a href

--- Asset/Scripts/main.js
+++ Asset/Scripts/main.js
@@ -10,3 +10,26 @@ var swiper = new Swiper(".swiper", {
     },
   },
 });
+
+//Counter
+let counterDisplayElem = document.querySelector("#counter-display");
+let counterMinusElem = document.querySelector("#counter-minus");
+let counterPlusElem = document.querySelector("#counter-plus");
+
+let count = 1;
+
+updateDisplay();
+
+counterPlusElem.addEventListener("click", () => {
+  count++;
+  updateDisplay();
+});
+
+counterMinusElem.addEventListener("click", () => {
+  count--;
+  updateDisplay();
+});
+
+function updateDisplay() {
+  counterDisplayElem.value = count;
+}
